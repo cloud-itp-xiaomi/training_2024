55a0793ea1d0e01df15a7cbb3b87e9fcb81a0b22
sunmaolin <3253636937@qq.com>
2024-05-27 16:35:21 +0800

    server模块初步实现

---
+++ sunmaolin/server/src/main/java/com/cl/server/ServerApplication.java
@@ -0,0 +1,21 @@
+package com.cl.server;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * @author: tressures
+ * @date: 2024-05-26 17:06:02
+ */
+@SpringBootApplication
+@ComponentScan("com.cl")
+@MapperScan("com.cl.server.mapper")
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class);
+    }
+
+}
