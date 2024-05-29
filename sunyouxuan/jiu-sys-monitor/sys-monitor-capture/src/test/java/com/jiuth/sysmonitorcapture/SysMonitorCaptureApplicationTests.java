84ad80ff4c7a4e8242160ade855fc34c1983b208
jiuxuanth <jiugexuan@foxmail.com>
2024-05-29 09:35:07 +0800

    孙友轩：作业采集模块和服务端模块提交 (#46)

---
+++ sunyouxuan/jiu-sys-monitor/sys-monitor-capture/src/test/java/com/jiuth/sysmonitorcapture/SysMonitorCaptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiuth.sysmonitorcapture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysMonitorCaptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
