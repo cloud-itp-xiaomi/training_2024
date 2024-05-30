a514190e715948130337474cad451e7428e0dc17
ahua1113 <115915162+ahua1113@users.noreply.github.com>
2024-05-30 09:49:08 +0800

    黄清华：初步实现collector和server (#50)
    * Create README.md
    
    * Create 新建个人目录.txt
    
    黄清华：创建个人目录
    
    * Delete 新建个人目录.txt
    
    * Create 新建.txt
    
    * Delete 新建.txt
    
    * 1
    
    * 2，收集器和server初步实现，mysql已连接，redis使用异常
    
    * 2，收集器和server初步实现，mysql已连接，redis使用异常
    
    * 11
    
    * 3

---
+++ huangqinghua/xiaomi1/src/main/java/com/example/xiaomi1/mapper/MetricMapper.java
@@ -0,0 +1,10 @@
+package com.example.xiaomi1.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.xiaomi1.entity.Metric;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MetricMapper extends BaseMapper<Metric> {
+
+}
