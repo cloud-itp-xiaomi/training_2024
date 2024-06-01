76fe1ef61491d3e1c510f5ba87b2cf0c54387315
qiaojingjing <1507988959@qq.com>
2024-06-01 12:26:40 +0800

    feat:常量

---
+++ qiaojingjing/LinuxMetricColl/Collector/src/main/java/org/qiaojingjing/cons/Param.java
@@ -0,0 +1,10 @@
+package org.qiaojingjing.cons;
+
+public class Param {
+    public static final String MEM = "mem.used.percent";
+    public static final String CPU = "cpu.used.percent";
+    public static final String MEMTOTAL = "MemTotal";
+    public static final String MEMFREE = "MemFree";
+    public static final String BUFFERS = "Buffers";
+    public static final String CACHED = "Cached";
+}
