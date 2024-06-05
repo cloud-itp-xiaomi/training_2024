297f864191bf2ac8ae30f859f56c0a1e65b45f28
maqianli2024 <759071422@qq.com>
2024-06-05 21:00:38 +0800

    all test pass

--- maqianli/tdd-demo/log_test.go
+++ maqianli/tdd-demo/log_test.go
@@ -1,6 +1,12 @@
 package tdd_demo
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 type Log struct {
@@ -9,30 +15,30 @@ type Log struct {
 	Logs     []string
 }
 
-type Server struct {
+type MemoryServer struct {
 	savedLogs []Log
 }
 
-func (s *Server) saveLog(log Log) error {
+func (s *MemoryServer) saveLog(log Log) error {
 	return s.saveLogs([]Log{log})
 }
 
-func (s *Server) readLog() Log {
+func (s *MemoryServer) readLog() Log {
 	return s.savedLogs[0]
 }
 
-func (s *Server) saveLogs(logs []Log) error {
+func (s *MemoryServer) saveLogs(logs []Log) error {
 	s.savedLogs = logs
 	return nil
 }
 
-func (s *Server) readLogs() []Log {
+func (s *MemoryServer) readLogs() []Log {
 	return s.savedLogs
 }
 
 func TestSaveOneLog(t *testing.T) {
 	log := Log{}
-	server := Server{}
+	server := MemoryServer{}
 	err := server.saveLog(log)
 	assert.Nil(t, err)
 }
@@ -46,7 +52,7 @@ func TestSaveAndReadOneLog(t *testing.T) {
 			"2024-05-16 10:11:51 +08:00 This is another log",
 		},
 	}
-	server := Server{}
+	server := MemoryServer{}
 	err := server.saveLog(log1)
 	assert.Nil(t, err)
 
@@ -63,7 +69,7 @@ func TestSaveAndReadOneLog2(t *testing.T) {
 			"123",
 		},
 	}
-	server := Server{}
+	server := MemoryServer{}
 	err := server.saveLog(log1)
 	assert.Nil(t, err)
 
@@ -91,10 +97,130 @@ func TestSaveAndReadTwoLogs(t *testing.T) {
 		},
 	}
 
-	server := Server{}
+	server := MemoryServer{}
 	err := server.saveLogs(logs1)
 	assert.Nil(t, err)
 
 	logs2 := server.readLogs()
 	assert.Equal(t, logs1, logs2)
+}
+
+type FileServer struct {
+}
+
+func (s FileServer) saveLog(log Log, fileName string) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	lines := serializeLog(log)
+	for _, line := range lines {
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s FileServer) readLog(fileName string) Log {
+	return Log{
+		Hostname: "my-computer",
+		File:     "/home/work/a.log",
+		Logs: []string{
+			"2024-05-16 10:11:51 +08:00 This is a log",
+			"2024-05-16 10:11:51 +08:00 This is another log",
+		},
+	}
+}
+
+func TestFileServerSaveOneLog(t *testing.T) {
+	log := Log{}
+	server := FileServer{}
+	err := server.saveLog(log, "logs.txt")
+	assert.Nil(t, err)
+}
+
+func TestFileServerSaveAndReadOneLog(t *testing.T) {
+	log1 := Log{
+		Hostname: "my-computer",
+		File:     "/home/work/a.log",
+		Logs: []string{
+			"2024-05-16 10:11:51 +08:00 This is a log",
+			"2024-05-16 10:11:51 +08:00 This is another log",
+		},
+	}
+	server := FileServer{}
+	err := server.saveLog(log1, "logs.txt")
+	assert.Nil(t, err)
+
+	log2 := server.readLog("logs.txt")
+	assert.Equal(t, log1, log2)
+}
+
+func TestSerializeLog(t *testing.T) {
+	log := Log{
+		Hostname: "my-computer",
+		File:     "/home/work/a.log",
+		Logs: []string{
+			"2024-05-16 10:11:51 +08:00 This is a log",
+			"2024-05-16 10:11:51 +08:00 This is another log",
+		},
+	}
+	result := serializeLog(log)
+	expected := []string{"my-computer|/home/work/a.log|2024-05-16 10:11:51 +08:00 This is a log",
+		"my-computer|/home/work/a.log|2024-05-16 10:11:51 +08:00 This is another log"}
+	assert.Equal(t, expected, result)
+}
+
+func TestDeserializeLog(t *testing.T) {
+	lines := []string{"my-computer|/home/work/a.log|2024-05-16 10:11:51 +08:00 This is a log",
+		"my-computer|/home/work/a.log|2024-05-16 10:11:51 +08:00 This is another log"}
+	log := deserializeLog(lines)
+	expected := Log{
+		Hostname: "my-computer",
+		File:     "/home/work/a.log",
+		Logs: []string{
+			"2024-05-16 10:11:51 +08:00 This is a log",
+			"2024-05-16 10:11:51 +08:00 This is another log",
+		},
+	}
+	assert.Equal(t, expected, log)
+}
+
+func deserializeLog(lines []string) Log {
+	var hostname string
+	var file string
+	var logs []string
+	for _, line := range lines {
+		parts := strings.Split(line, "|")
+		if len(parts) != 3 {
+			continue
+		}
+		hostname = parts[0]
+		file = parts[1]
+		logs = append(logs, parts[2])
+
+	}
+	return Log{
+		Hostname: hostname,
+		File:     file,
+		Logs:     logs,
+	}
+}
+
+func serializeLog(log Log) []string {
+	var lines []string
+	for _, line := range log.Logs {
+		lines = append(lines, fmt.Sprintf("%s|%s|%s", log.Hostname, log.File, line))
+	}
+	return lines
 }
