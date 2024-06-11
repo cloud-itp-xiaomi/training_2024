9a18185a9842c5e8d53c36cdcdb1258462216f07
Chocolate2323 <3104554724@qq.com>
2024-06-11 19:31:05 +0800

    编码规范示例

---
+++ xuteng/codingStyle/study.java
@@ -0,0 +1,43 @@
+public class Study1 {
+    public static List<File> getUserDirectory(String path, List<File> list) {
+        File file = new File(path);
+
+        if (!file.exists()) {
+            System.out.println("文件不存在!");
+            return list;
+        }
+
+        if (file.getName().contains("不算")) {
+            return list;
+        }
+
+        if (file.isDirectory() && file.getName().matches("\\d+")) {
+            list.add(file);
+            return list;
+        }
+
+        File[] files = file.listFiles();
+        if (files.length == 0) {
+            System.out.println("文件夹是空的!");
+            return list;
+        }
+
+        for (File file2: files) {
+            if (file2.getName().contains("不算")) {
+                continue;
+            }
+
+            if (!fiel2.isDirectory()) {
+                continue;
+            }
+
+            if (file2.getName().matches("\\d+")) {
+                list.add(file2);
+            } else {
+                getUserDirectory(file2.getAbsolutePath(), list);
+            }
+        }
+
+        return list;
+    }
+}
