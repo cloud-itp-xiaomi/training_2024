bff83f36b96654f57925570a9ebf367098f32ff6
zhoujiao1126 <17371526870@163.com>
2024-06-11 18:31:42 +0800

    Update ModifyExample.java

--- zhoujiao/modify-example/ModifyExample.java
+++ zhoujiao/modify-example/ModifyExample.java
@@ -1,41 +1,75 @@
-import java.io.File;
-import java.util.List;
 
-public class Study1 {
+public class ModifyExample {
 
-    /**
-     * 递归地获取指定路径下符合条件的目录，并将其添加到列表中。
-     *
-     * @param path 目录或文件的路径
-     * @param list 存储符合条件的目录的列表
-     * @return 符合条件的目录列表
-     */
-    public static List<File> getUserDirectory(String path, List<File> list) {
-        File file = new File(path);
-        if (file.exists()) {
-            if (file.getName().contains("不算")) {
+        public static List<File> getUserDirectoryNew(String path, List < File > list) {
+            File file = new File(path);
+            if(!file.exists()) {
+                System.out.println("文件不存在!");
                 return list;
             }
-            if (file.isDirectory() && file.getName().matches("\\d+")) {
+
+            if(file.getName().contains("不算")) {
+                System.out.println("非生产文件!");
+                return list;
+            }
+
+            if(file.isDirectory() && file.getName().matches("\\d+")) {
                 list.add(file);
-            } else {
-                // 递归处理子目录和文件
-                File[] files = file.listFiles();
-                if (files != null && files.length > 0) { // 确保files不为null
-                    for (File file2 : files) {
-                        if (file2.getName().contains("不算")) {
-                            continue;
-                        }
-                        getUserDirectory(file2.getAbsolutePath(), list);
-                    }
-                } else {
-                    System.out.println("文件夹是空的!");
+                return list;
+            }
+
+            File[] files = file.listFiles();
+            if(files.length == 0) {
+                System.out.println("文件夹是空的!");
+                return list;
+            }
+            for(File file2: files) {
+                if(file2.getName().contains("不算")) {
+                    continue;
+                }
+
+                if(file2.isDirectory() && !file2.getName().matches("\\d+")) {
+                    getUserDirectory2(file2.getAbsolutePath(), list);
+                    continue;
+                }
+
+                if (file2.isDirectory() && file2.getName().matches("\\d+")) {
+                    list.add(file2);
                 }
             }
-        } else {
-            System.out.println("文件不存在!");
-        }
-        return list;
-    }
 
+            return list;
+        }
+
+        public static List< File > getUserDirectoryOld(String path, List < File > list) {
+            File file = new File(path);
+            if(file.exists()) {
+                if(file.getName().contains("不算")) {
+                    return list;
+                }
+                if(file.isDirectory() && file.getName().matches("\\d+")) {
+                    list.add(file);
+                } else {
+                    File[] files = file.listFiles();
+                    if(files.length == 0) {
+                        System.out.println("文件夹是空的!");
+                    } else {
+                        for(File file2: files) {
+                            if(file2.getName().contains("不算")) {
+                                continue;
+                            } else if(file2.isDirectory() && !file2.getName().matches("\\d+")) {
+                                getUserDirectory(file2.getAbsolutePath(), list);
+                            } else if(file2.isDirectory() && file2.getName().matches("\\d+")) {
+                                list.add(file2);
+                            }
+                        }
+                    }
+                }
+            } else {
+                System.out.println("文件不存在!");
+            }
+            return list;
+        }
 }
+
+
