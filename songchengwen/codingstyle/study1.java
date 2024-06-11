c5ba0dd568e1970ecc64543eb61eebf3ef5e1424
Chengwen.Song <2793132875@qq.com>
2024-06-11 12:40:50 +0800

    new codingstyle1

--- songchengwen/codingstyle/study1.java
+++ songchengwen/codingstyle/study1.java
@@ -1,32 +1,32 @@
 public class Study1 {
-    public static List< File > getUserDirectoryOld(String path, List < File > list) {
-         File file = new File(path);
-         if(file.exists()) {
-             if(file.getName().contains("不算")) {
-                 return list;
-             }
-             if(file.isDirectory() && file.getName().matches("\\d+")) {
-                 list.add(file);
-             } else {
-                 File[] files = file.listFiles();
-                 if(files.length == 0) {
-                     System.out.println("文件夹是空的!");
-                 } else {
-                     for(File file2: files) {
-                         if(file2.getName().contains("不算")) {
-                             continue;
-                         } else if(file2.isDirectory() && !file2.getName().matches("\\d+")) {
-                             getUserDirectory(file2.getAbsolutePath(), list);
-                         } else if(file2.isDirectory() && file2.getName().matches("\\d+")) {
-                             list.add(file2);
-                         }
-                     }
-                 }
-             }
-         } else {
-             System.out.println("文件不存在!");
-         }
-         return list;
+    public static List<File> getUserDirectoryOld(String path, List<File> list) {
+        File file = new File(path);
+        if(file.exists()) {
+            if(file.getName().contains("不算")) {
+                return list;
+            }
+            if(file.isDirectory() && file.getName().matches("\\d+")) {
+                list.add(file);
+            } else {
+                File[] files = file.listFiles();
+                if(files == null || files.length == 0) {
+                    System.out.println("文件夹是空的!");
+                } else {
+                    for(File file2: files) {
+                        if(file2.getName().contains("不算")) {
+                            continue;
+                        } else if(file2.isDirectory() && !file2.getName().matches("\\d+")) {
+                            getUserDirectoryOld(file2.getAbsolutePath(), list);
+                        } else if(file2.isDirectory() && file2.getName().matches("\\d+")) {
+                            list.add(file2);
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("文件不存在!");
+        }
+        return list;
     }
 
 	public static List< File > getUserDirectoryNew(String path, List < File > list) {
@@ -49,7 +49,7 @@ public class Study1 {
 		}
 		for (File file2 : files) {
 			if (file2.isDirectory() && !file.getName().contains("不算")) {
-				getUserDirectory(file2.getAbsolutePath(), list);
+				getUserDirectoryNew(file2.getAbsolutePath(), list);
 			}
 		}
     }
