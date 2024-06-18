eda1b78a826b539d08f1188e4e534824fe4a1046
Chengwen.Song <2793132875@qq.com>
2024-06-18 16:59:24 +0800

    commit HomeWork01

---
+++ songchengwen/UtilizationCollection/server/src/main/java/com/h_s/config/RedisConfig.java
@@ -0,0 +1,29 @@
+// RedisConfig.java
+package com.h_s.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // 使用StringRedisSerializer序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        // 使用GenericJackson2JsonRedisSerializer序列化和反序列化redis的value值
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
