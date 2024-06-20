411dddcf0561a8e167e189dc06a8f6c30890d8aa
ahua1113 <115915162+ahua1113@users.noreply.github.com>
2024-06-20 17:18:20 +0800

    一些小改动

---
+++ huangqinghua/xiaomi1/src/test/java/com/example/xiaomi1/service/LogServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.xiaomi1.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LogServiceTest {
+
+    @Test
+    void saveLogMysql() {
+    }
+
+    @Test
+    void saveLogLocal() {
+    }
+
+    @Test
+    void getLog() {
+    }
+
+    @Test
+    void getLogByMysql() {
+    }
+
+    @Test
+    void getLogByLocal() {
+    }
+}
