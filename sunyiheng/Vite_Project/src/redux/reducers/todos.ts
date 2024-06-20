e58b7eed31c1cd81362e7981b33b621277eedd91
syh <807968172@qq.com>
2024-06-20 09:39:22 +0800

    根据action的动作类型，实现向待办列表增加新待办，以及根据id来更改待办的完成状态功能

--- sunyiheng/Vite_Project/src/redux/reducers/todos.ts
+++ sunyiheng/Vite_Project/src/redux/reducers/todos.ts
@@ -8,7 +8,7 @@ import { ADD_TODO, TOGGLE_TODO } from "../constant";
  * }
  *
  * 作用：1. 向待办事项列表添加新的待办事项;
- *       2. 根据id来更改待办事项的完成状态
+ *       2. 根据id来更改待办事项的完成状态。
  */
 const iniState = []; // ToDoList，初始状态为[]，根据旧的state和action，返回新的state
 export default function (previousState = iniState, action) {
