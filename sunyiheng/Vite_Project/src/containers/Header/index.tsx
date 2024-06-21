a73a0491b342202095befed1c75d35b8054922d3
syh <807968172@qq.com>
2024-06-21 16:44:11 +0800

    实现Header为受控组件

--- sunyiheng/Vite_Project/src/containers/Header/index.tsx
+++ sunyiheng/Vite_Project/src/containers/Header/index.tsx
@@ -1,34 +1,47 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { add_todo } from "../../redux/actions/";
 import { connect } from "react-redux";
 import { nanoid } from "nanoid";
+import { Button, Input, Space } from "antd";
+import "./index.css";
 
-function Header(props) {
-  const { todos, add_todo } = props;
-
-  const inputRef = useRef(null);
+function Header({ add_todo }) {
+  // 受控组件的实现
+  const [inputValue, setInputValue] = useState("");
 
   const addTodo = () => {
-    const todo = inputRef.current.value;
     const newTodo = {
       id: nanoid(),
-      todo,
+      todo: inputValue,
       completed: false,
     };
-    if (todo.trim() === "") {
+    if (inputValue.trim() === "") {
       alert("输入不能为空");
       return;
     }
     add_todo(newTodo);
-    inputRef.current.value = "";
+    setInputValue("");
   };
 
   return (
     <>
+      <div className="title">
+        <h1>带有撤销功能的待办事项</h1>
+        <img src="待办事项.svg"></img>
+      </div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" ref={inputRef} />
+        {/* <input type="text" ref={inputRef} />
         &nbsp;
-        <button onClick={addTodo}>点击添加待办事项</button>
+        <button onClick={addTodo}>点击添加待办事项</button> */}
+        <Space.Compact style={{ width: "100%" }}>
+          <Input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button type="primary" onClick={addTodo}>
+            点击添加待办事项
+          </Button>
+        </Space.Compact>
       </form>
     </>
   );
